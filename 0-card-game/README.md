b250b4c7156f4d2f947a60d7819a61010105452a
chloexu <xuzhuqing1124@gmail.com>
2022-06-09 15:05:29 -0400

    First commit on card game with goroutine

---
+++ 0-card-game/README.md
@@ -0,0 +1,22 @@
+## About
+
+This is a card game written in golang.
+
+This game currently takes 5 hardcoded players, there are 10 rounds. For each round, each player plays by drawing a card with number ranging 1-13. Based on the card number, player obtains a score for the round. Scores for all 10 rounds will be summed up by each player and will be used to determine who is the winner of the game, whoever has the highest total score will be the winner.
+
+This program is a practice for using goroutine and channel communication in golang. 
+
+In the code, there are 2 channels defined, one for emitting all players' score for each round to a monitor that prints out the result of the round, the other for letting the monitor thread know that all round has finished so it could announce the winner and exit the monitoring.
+
+In the code there are 2 threads running, one of them is a goroutine that starts the game and have all players play each round, other than printing the log on starting the round, this goroutine is purely in charge of organizing the game and coordinate it to proceed, while the other thread the `Monitor` function is in charge of announcing all the game results for each round and the final winner information.
+
+
+## Run the game
+Go to `./0-card-game/` directory in local and run:
+```
+go run main.go
+```
+
+
+## Next steps
+As the next steps of this game, I plan to write a command line tool that lets user enter the number of players and the name of each player before starting the game, instead of using hardcoded players.
---
+++ 0-card-game/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Player struct {
+	name        string
+	latestDraw  int
+	latestScore int
+	totalScore  int
+}
+
+func (p *Player) Play() {
+	rand.Seed(time.Now().UnixNano())
+	draw := rand.Intn(13) + 1
+	var rs int
+	switch draw {
+	case 1, 3, 5:
+		rs = 5
+	case 2, 4:
+		rs = 2
+	case 6, 8:
+		rs = -8
+	case 7:
+		rs = -7
+	case 9, 10:
+		rs = 2
+	case 11, 12:
+		rs = 5
+	case 13:
+		rs = -9
+	}
+	p.latestDraw = draw
+	p.latestScore = rs
+	p.totalScore += rs
+}
+
+func EndRound(players []*Player, c chan *Player) {
+	for _, player := range players {
+		c <- player
+	}
+}
+
+func Monitor(c chan *Player, q chan *Player) {
+	for {
+		select {
+		case player := <-c:
+			fmt.Printf("-------- Player %v: | draw [%v] | score [%v] | ====> total score %v\n",
+				player.name, player.latestDraw, player.latestScore, player.totalScore)
+		case winner := <-q:
+			fmt.Printf("\n************* Game over! Winner is %v with a total score of %v, Congrats!\n\n",
+				winner.name, winner.totalScore)
+			return
+		}
+	}
+}
+
+func AnounceWinner(players []*Player, q chan *Player) {
+	var winner *Player
+	winnerScore := -99999
+	for _, player := range players {
+		if player.totalScore > winnerScore {
+			winner = player
+			winnerScore = player.totalScore
+		}
+	}
+	q <- winner
+}
+
+func main() {
+	c := make(chan *Player)
+	q := make(chan *Player)
+	players := []*Player{
+		{name: "Chloe", totalScore: 0},
+		{name: "Peixuan", totalScore: 0},
+		{name: "Chen", totalScore: 0},
+		{name: "Waz", totalScore: 0},
+		{name: "Laurence", totalScore: 0},
+	}
+	rounds := make([]int, 10)
+
+	go func() {
+		for r := range rounds {
+			fmt.Printf("\n************* About to start Round #%v *************\n\n", r+1)
+			// for each round, have each player start the round to get the score
+			for _, player := range players {
+				player.Play()
+			}
+			// then end the round after all players finish playing
+			EndRound(players, c)
+			// sleep for 5 seconds before proceeding to the next round
+			time.Sleep(1 * time.Second)
+		}
+		AnounceWinner(players, q)
+	}()
+	Monitor(c, q)
+}
